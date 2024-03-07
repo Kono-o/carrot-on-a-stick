13b0e46dff0d6b291a1aa005b30d4ad463ad7488
Kono <75730503+Kono-o@users.noreply.github.com>
2024-03-07 23:32:47 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate dirs;
-use std::{env, fs};
+use std::fs;
+use std::env;
 
-const RES_DIR_LINUX: &str = r"/.minecraft/resourcepacks/";
-const RES_DIR_WINDOWS: &str = r"\AppData\Roaming\.minecraft\resourcepacks\";
+const RES_DIR_LINUX: &str = "/.minecraft/resourcepacks/";
+const RES_DIR_WINDOWS: &str = "/AppData/Roaming/.minecraft/resourcepacks/";
 const TARGET_PATH: &str = "./packs/";
 
 fn main() {
@@ -17,17 +18,19 @@ fn main() {
         "windows" => packs_path = format!("{}{}", home_path, RES_DIR_WINDOWS),
         _ => println!("{} not supported", env::consts::OS)
     }
+    println!("{}", packs_path);
     let paths = fs::read_dir(packs_path).unwrap();
     
     for path in paths {
         let path = path.unwrap().path().display().to_string();
         let current_path = std::path::Path::new(&path);
-        if !path.ends_with("zip") {continue};
+        if !(path.ends_with(".zip") || path.ends_with(".mcpack")) { continue };
         
         let current_pack = fs::File::open(current_path).unwrap();
         let mut pack_name: &str = <&str>::try_from(current_path.file_name().unwrap()).unwrap();
-        println!("extracting {} to /packs/", pack_name);
-        pack_name = pack_name.strip_suffix(".zip").unwrap();
+        println!("extracting {}", pack_name);
+        if pack_name.ends_with(".zip") {pack_name = pack_name.strip_suffix(".zip").unwrap();}
+        if pack_name.ends_with(".mcpack") {pack_name = pack_name.strip_suffix(".mcpack").unwrap();}
         
         let current_target_path = format!("{}{}/",TARGET_PATH,pack_name);
         let mut archive = zip::ZipArchive::new(current_pack).unwrap();
