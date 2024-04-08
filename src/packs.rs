ba7d420cae86d50efcf5f53950bc5590cbd6d021
Kono <75730503+Kono-o@users.noreply.github.com>
2024-04-08 22:38:08 +0530

    refactor

--- src/packs.rs
+++ src/packs.rs
@@ -1,5 +1,6 @@
 use concat_string::concat_string;
 use std::{fs,path::Path};
+use std::ffi::OsStr;
 use dircpy::CopyBuilder;
 use zip::ZipArchive;
 
@@ -14,51 +15,42 @@ pub fn get_packs(packs_dir: &Path) -> Result<(),PackErrorType> {
   if !packs_dir.exists() { return Err(PackErrorType::MissingDir) }
   for pack in fs::read_dir(&packs_dir).unwrap() {
     let pack_dir = pack.unwrap().path();
+    let pack_type = match pack_dir.extension() {
+      Some(ex) => ex.to_str().unwrap(),
+      None => "folder"
+    };
     let pack_name = pack_dir.file_stem().unwrap().to_str().unwrap();
-    log::msg(&concat_string!("reading [" ,pack_name, "]..."));
+    log::msg(&concat_string!("reading [", pack_name, "(", pack_type, ")]..."));
     let out_dir = concat_string!(global::PACKS_OUT_PATH,pack_name,"/");
-    match pack_dir.extension() {
-      Some(ex) => try_unzip(&pack_dir, &pack_name, &ex.to_str().unwrap(), &out_dir),
-      None => try_copy(&pack_dir, &pack_name, &out_dir),
-    }
+    check_and_copy(&pack_dir, &out_dir);
   }
   Ok(())
 }
 
-fn try_copy(dir: &Path, out: &str) {
-  let je_tex_dir = Path::new(dir).join(global::TEXTURES_PATHS[0]);
-  let be_tex_dir = Path::new(dir).join(global::TEXTURES_PATHS[1]);
-  if !(je_tex_dir.exists() || be_tex_dir.exists()) {
-    log::msg(&"has no textures");
-    return;
-  }
-  CopyBuilder::new(&dir, out)
-    .overwrite_if_newer(true).overwrite_if_size_differs(true)
-    .run().unwrap();
-  log::msg(&concat_string!("was copied to ", out));
-}
-
-fn try_unzip(dir: &Path, ex: &str, out: &str) {
-  if !is_valid_extension(ex) {
-    log::msg(&concat_string!("not a resource pack"));
-    return;
-  }
-  ZipArchive::new(fs::File::open(&dir).unwrap()).unwrap().extract(&out).unwrap();
-  log::msg(&concat_string!("unzipped to ", out));
-}
-
-fn has_textures(dir: &Path) {
-  let je_tex_dir = Path::new(dir).join(global::TEXTURES_PATHS[0]);
-  let be_tex_dir = Path::new(dir).join(global::TEXTURES_PATHS[1]);
-  if !(je_tex_dir.exists() || be_tex_dir.exists()) {
-    log::msg(&"has no textures");
-    return;
+fn check_and_copy(dir: &Path, out: &str) {
+  match dir.extension() {
+    Some(ex) if is_zip(ex)  => {
+      ZipArchive::new(fs::File::open(&dir).unwrap())
+        .unwrap().extract(&out).unwrap();
+      log::msg(&concat_string!("unzipped to ", out));
+    }
+    _ => {
+      let (je_mcmeta, be_manif) =
+        (Path::new(dir).join(global::PACK_DESC_FILES[0]),
+         Path::new(dir).join(global::PACK_DESC_FILES[1]));
+      if je_mcmeta.exists() || be_manif.exists() {
+        CopyBuilder::new(&dir, out)
+          .overwrite_if_size_differs(true).overwrite_if_newer(true)
+          .run().unwrap();
+        log::msg(&concat_string!("copied to ", out));
+      }
+    }
   }
 }
 
-fn is_valid_extension(ex: &str) -> bool {
+fn is_zip(ex: &OsStr) -> bool {
   for extension in global::PACK_EXTENSIONS.iter() {
     if ex.eq(*extension) { return true }
   }
-  false
+false
 }
